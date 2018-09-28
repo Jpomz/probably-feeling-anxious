bb67fda4fa398d87192d3c4c7c5ff2e1e17c4f49
Justin Pomeranz <jfpomeranz@gmail.com>
2018-09-28 12:02:25 -0600

    update figures to panels, change AMD axis

--- fw_measure_stats.R
+++ fw_measure_stats.R
@@ -5,37 +5,47 @@ library(ggplot2)
 library(robustbase)
 library(lmtest)
 library(forcats)
+library(gridExtra)
 
 dat <- readRDS("data/stability_results.RDS") %>%
   filter(.id == "random")
 
+# species stats
+# filtering out one observation of each S:pca1 combination
+S.dat <- dat %>%
+  select(S, pca1, Site) %>%
+  distinct
+bptest(S~pca1, data = S.dat)
+anova(lm(S~pca1, data = S.dat),
+      lm(S~pca1 +I(pca1^2), data = S.dat))
+AIC(lm(S~pca1, data = S.dat),
+    lm(S~pca1 +I(pca1^2), data = S.dat))
+# AIC gives both models equal weight
+# going with 
+S.mod <- lm(S~pca1 +I(pca1^2), data = S.dat)
+summary(S.mod)
+
 # heteroskedasticity test format
-# All response variables are heteroskedastic
-bptest(S~pca1, data = dat)
+# All response variables are (Except Species no.) heteroskedastic
+bptest(L~pca1, data = dat)
 
 # robust regressions
 # testing for linear vs quadratic relationship
 # anova result if p <0.05 then inclusion of additional terms (e.g. quadratic term) appropriate
-anova(lmrob(S~pca1, data = dat),
-      lmrob(S~pca1 +I(pca1^2), data = dat),
-      test = "Wald")
-S.mod <- lmrob(S~pca1 +I(pca1^2), data = dat)
-summary(S.mod)
-
 anova(lmrob(L~pca1, data = dat, 
             control = lmrob.control(
               maxit.scale = 700,
-              rel.tol = 1e-6)),
+              rel.tol = 1e-5)),
       lmrob(L~pca1 +I(pca1^2), data = dat,
             control = lmrob.control(
               maxit.scale = 700,
-              rel.tol = 1e-6)),
+              rel.tol = 1e-5)),
       test = "Wald")
 L.mod <- lmrob(L~pca1 +I(pca1^2),
                data = dat,
                control = lmrob.control(
                  maxit.scale = 700,
-                 rel.tol = 1e-6))
+                 rel.tol = 1e-5))
 summary(L.mod)
 
 anova(lmrob(C~pca1, data = dat),
@@ -104,12 +114,12 @@ fig_fun <- function(y,ylab,
                     rel.tol = 1e-07,
                     linetype = "solid",
                     linecolor = "black"){
-  ggplot(data, aes(x = data[[x]], y = data[[y]])) +
+  ggplot(data, aes(x = -data[[x]], y = data[[y]])) +
     geom_point(aes(color = fct_reorder(Site, pca1)))+
     theme_bw()+
     theme(legend.position = "none")+
     scale_color_discrete(h.start = 80) +
-    labs(x = "Mining gradient", y = ylab)+
+    labs(x = "AMD gradient", y = ylab)+
     if(quad == TRUE)
       stat_smooth(method = "lmrob",
                   formula = y~x+I(x^2),
@@ -130,28 +140,63 @@ fig_fun <- function(y,ylab,
                              rel.tol = rel.tol))) 
 }
 
-fig_fun(y = "S", ylab = "No. Species")
-ggsave("figures/S~gradient.png")
-fig_fun(y = "L", ylab = "No. Links",
+# fig 1A-C ####
+ggplot(S.dat, aes(x = -pca1, y = S)) +
+  geom_point(aes(color = fct_reorder(Site, pca1)))+
+  theme_bw()+
+  theme(legend.position = "none")+
+  scale_color_discrete(h.start = 80) +
+  labs(x = "AMD gradient", y = "No. Species") +
+  stat_smooth(method = "lm", 
+              formula = y~x+I(x^2),
+              color = "black") +
+    annotate(geom = "text", x = -5.6, y = 33,
+             label = "A", size = 6))
+
+#ggsave("figures/S~gradient.png")
+L.fig <- fig_fun(y = "L", ylab = "No. Links",
         maxit.scale = 700,
-        rel.tol = 1e-6)
-ggsave("figures/L~gradient.png")
-fig_fun(y = "C", ylab = "Connectance")
-ggsave("figures/C~gradient.png")
+        rel.tol = 1e-5) +
+  annotate(geom = "text", x = -5.6, y = 150,
+           label = "B", size = 6)
+#ggsave("figures/L~gradient.png")
+C.fig <- fig_fun(y = "C", ylab = "Connectance") +
+  annotate(geom = "text", x = -5.6, y = 0.3,
+           label = "C", size = 6)
+#ggsave("figures/C~gradient.png")
+
+tiff(filename = "figures/fig_1.tiff",
+     width = 190, height = 210, units = "mm", res =300)
+grid.arrange(S.fig, L.fig, C.fig, ncol = 1)
+dev.off()
+
+
 fig_fun(y = "B", ylab = "Prop. Basal",
         linetype = "dashed",
         linecolor = "red")
-ggsave("figures/B~gradient.png")
-fig_fun(y = "I", ylab = "Prop. Intermediate")
-ggsave("figures/I~gradient.png")
-fig_fun(y = "T", ylab = "Prop. Top", quad = FALSE)
-ggsave("figures/T~gradient.png")
-fig_fun(y = "Vulsd", ylab = "Vul SD",
-        maxit.scale = 700, rel.tol = 1e-6)
-ggsave("figures/Vul~gradient.png")
-fig_fun(y = "Gensd", ylab = "Gen SD",
-        maxit.scale = 700, rel.tol = 1e-6)
-ggsave("figures/Gen~gradient.png")
+#ggsave("figures/B~gradient.png")
 
+# fig 2 A-D
+I.fig <- fig_fun(y = "I", ylab = "Prop. Intermediate") +
+  annotate(geom = "text", x = -5.6, y = 0.9,
+           label = "A", size = 6)
 
+T.fig <- fig_fun(y = "T", ylab = "Prop. Top", quad = FALSE)+
+  annotate(geom = "text", x = -5.6, y = 0.75,
+           label = "B", size = 6)
+
+V.fig <- fig_fun(y = "Vulsd", ylab = "Vul SD",
+        maxit.scale = 700, rel.tol = 1e-6)+
+  annotate(geom = "text", x = -5.6, y = 0.17,
+           label = "C", size = 6)
+
+G.fig <- fig_fun(y = "Gensd", ylab = "Gen SD",
+        maxit.scale = 700, rel.tol = 1e-6) +
+  annotate(geom = "text", x = -5.6, y = 0.17,
+           label = "D", size = 6)
+
+tiff(filename = "figures/fig_2.tiff",
+     width = 190, height = 190, units = "mm", res =300)
+grid.arrange(I.fig, T.fig, V.fig, G.fig, ncol = 2)
+dev.off()
 
--- stability_stats.R
+++ stability_stats.R
@@ -1,10 +1,12 @@
 # Stability statistics
+library(plyr)
 library(dplyr)
 library(purrr)
 library(ggplot2)
 library(robustbase)
 library(lmtest)
 library(forcats)
+library(gridExtra)
 
 dat <- readRDS("data/stability_results.RDS") %>%
   split(list(.$.id))
@@ -23,12 +25,12 @@ dat %>% map(~summary(lmrob(stab~pca1 +I(pca1^2), data = .x)))
 
 fig_fun <- function(data, mainlab,
                     x = "pca1", y = "stab"){
-  ggplot(data, aes(x = data[[x]], y = data[[y]])) +
+  ggplot(data, aes(x = -data[[x]], y = data[[y]])) +
     geom_point(aes(color = fct_reorder(Site, pca1)))+
     theme_bw()+
     theme(legend.position = "none")+
     scale_color_discrete(h.start = 80) +
-    labs(x = "Mining gradient",
+    labs(x = "AMD gradient",
          y = "stability metric",
          title = mainlab)+
     stat_smooth(method = "lmrob",
@@ -36,11 +38,97 @@ fig_fun <- function(data, mainlab,
                 color = "black") 
 }
 
-fig_fun(data = dat$random, mainlab = "Random")
-ggsave("figures/s~random.png")
-fig_fun(data = dat$scaled, mainlab = "Scaled")
-ggsave("figures/s~scaled.png")
-fig_fun(data = dat$corr, mainlab = "Correlated")
-ggsave("figures/s~correlated.png")
-fig_fun(data = dat$s.c, mainlab = "Scaled & correlated")
-ggsave("figures/s~s_c.png")
+# Fig 3A-D
+fig.3a <- fig_fun(data = dat$random, mainlab = "Random") +
+  annotate(geom = "text", x = -5.6, y = 0.22, 
+           label = "A", size = 6)
+fig.3b <- fig_fun(data = dat$scaled, mainlab = "Scaled") +
+  annotate(geom = "text", x = -5.6, y = 0.075,
+           label = "B", size = 6)
+fig.3c <- fig_fun(data = dat$corr, mainlab = "Correlated") +
+  annotate(geom = "text", x = -5.6, y = 0.17,
+           label = "C", size = 6)
+fig.3d <- fig_fun(data = dat$s.c, mainlab = "Scaled & correlated") +
+  annotate(geom = "text", x = -5.6, y = 0.11,
+           label = "D", size = 6)
+
+tiff(filename = "figures/fig_3.tiff",
+     width = 190, height = 190, units = "mm", res =300)
+grid.arrange(fig.3a, fig.3b, fig.3c, fig.3d, ncol = 2)
+dev.off()
+
+dat.df <- ldply(dat)
+
+ggplot(dat.df, aes(y = stab, x = pca1, color = .id)) +
+  geom_point() +
+  stat_smooth(method = "lmrob",
+              formula = y~x+I(x^2))
+
+dat.df %>% filter(.id == "random" | .id == "corr") %>%
+ggplot(aes(y = stab, x = pca1, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+
+# random vs sc
+dat.df %>% filter(.id == "random" | .id == "s.c") %>%
+  ggplot(aes(y = stab, x = pca1, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+
+# only look at unimpacted sites
+dat.df %>% filter(pca1 < -1,
+  .id == "random" | .id == "s.c") %>%
+  ggplot(aes(y = stab, x = pca1, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+
+# stability ~ S
+dat.df %>% 
+  ggplot(aes(y = stab, x = S, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lmrob",
+              formula = y~x+I(x^2)) +
+  theme_bw()
+
+# stability ~ S
+# slight decrease in stability as size increases
+dat.df %>% filter(pca1 < -1) %>%
+  ggplot(aes(y = stab, x = S, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lmrob",
+              formula = y~x+I(x^2)) +
+  theme_bw()
+
+# stability ~ max.tl
+dat.df %>% filter(pca1 < -1,
+                  !is.na(max.TL)) %>%
+  ggplot(aes(y = stab, x = log10(max.TL), color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+
+# impacted sites
+# Size
+dat.df %>% filter(pca1 > -1) %>%
+  ggplot(aes(y = stab, x = S, color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
+# max.TL
+dat.df %>% filter(pca1 > -1,
+                  !is.na(max.TL)) %>%
+  ggplot(aes(y = stab, x = log10(max.TL), color = .id)) +
+  geom_point(position = position_jitter(width = 0.3),
+             alpha = 0.1) +
+  stat_smooth(method = "lm") +
+  theme_bw()
